492376fdc88d38e40f416b831f94b91df53bae98
Elzodbek <elzodbekahmedov@gmail.com>
2026-03-24 09:52:40 +0500

    full fix

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,13 @@
-const CACHE = 'goalflow-v2'
-const BASE = '/4-oylik-chalange-men-uchun'
+const CACHE = 'goalflow-v3'
 
 self.addEventListener('install', e => { self.skipWaiting() })
-self.addEventListener('activate', e => { 
+self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys => 
+    caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
     )
   )
-  self.clients.claim() 
+  self.clients.claim()
 })
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
