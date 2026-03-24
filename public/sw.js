11534aa3d5e75d43291ee9b50bdf8ca2eccd2800
Elzodbek <elzodbekahmedov@gmail.com>
2026-03-24 09:46:20 +0500

    final bug fix mobile

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,15 @@
-const CACHE = 'goalflow-v1'
+const CACHE = 'goalflow-v2'
+const BASE = '/4-oylik-chalange-men-uchun'
+
 self.addEventListener('install', e => { self.skipWaiting() })
-self.addEventListener('activate', e => { self.clients.claim() })
+self.addEventListener('activate', e => { 
+  e.waitUntil(
+    caches.keys().then(keys => 
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  )
+  self.clients.claim() 
+})
 self.addEventListener('fetch', e => {
   e.respondWith(fetch(e.request).catch(() => caches.match(e.request)))
 })
